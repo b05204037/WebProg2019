86145d9a9f7f2c75242deb7aa1b5c27a9a913471
b05204037 <42628966+b05204037@users.noreply.github.com>
2019-03-24 14:55:53 +0800

    Add files via upload

---
+++ Practice03/own/src/APP.js
@@ -0,0 +1,30 @@
+import React from "react";
+import Title from "./components/Title";
+import Contain from "./components/Content";
+function APP() {
+  return (
+    <div>
+      <Title />
+      <Contain
+        classN="wrapper-post-bg-img1"
+        place="Nibh non lobortis mus nibh"
+        time="01.01.2017"
+        main="Scelerisque enim mi curae erat ultricies lobortis donec velit in per cum consectetur purus a enim platea vestibulum lacinia et elit ante scelerisque vestibulum. At urna condimentum sed vulputate a duis in senectus ullamcorper lacus cubilia consectetur odio proin sociosqu a parturient nam ac blandit praesent aptent. Eros dignissim mus mauris a natoque ad suspendisse nulla a urna in tincidunt tristique enim arcu litora scelerisque eros suspendisse."
+      />
+      <Contain
+        classN="wrapper-post-bg-img2"
+        place="Mus elit a ultricies at"
+        time="12.21.2016"
+        main="Scelerisque enim mi curae erat ultricies lobortis donec velit in per cum consectetur purus a enim platea vestibulum lacinia et elit ante scelerisque vestibulum. At urna condimentum sed vulputate a duis in senectus ullamcorper lacus cubilia consectetur odio proin sociosqu a parturient nam ac blandit praesent aptent. Eros dignissim mus mauris a natoque ad suspendisse nulla a urna in tincidunt tristique enim arcu litora scelerisque eros suspendisse."
+      />
+      <Contain
+        classN="wrapper-post-bg-img3"
+        place="Varius a cursus aliquet"
+        time="11.11.2016"
+        main="Scelerisque enim mi curae erat ultricies lobortis donec velit in per cum consectetur purus a enim platea vestibulum lacinia et elit ante scelerisque vestibulum. At urna condimentum sed vulputate a duis in senectus ullamcorper lacus cubilia consectetur odio proin sociosqu a parturient nam ac blandit praesent aptent. Eros dignissim mus mauris a natoque ad suspendisse nulla a urna in tincidunt tristique enim arcu litora scelerisque eros suspendisse."
+      />
+    </div>
+  );
+}
+
+export default APP;
---
+++ Practice03/own/src/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import APP from "./APP.js";
+
+ReactDOM.render(<APP />, document.getElementById("root"));
